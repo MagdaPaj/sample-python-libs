4d6f95199c7eb6401ad1cc4d5c4e87ff99dffe5b
Magdalena Baran <mbaran@microsoft.com>
2024-06-06 11:37:22 +0200

    Add a new exception

--- src/custom_exceptions_lib/exceptions.py
+++ src/custom_exceptions_lib/exceptions.py
@@ -8,3 +8,9 @@ class CustomException3(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+class CustomException4(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
